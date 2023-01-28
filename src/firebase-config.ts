50fc8ae06e6b01d71d4ad684045b5edc4f909865
Harley Harris <harley.harris@digital.cabinet-office.gov.uk>
2023-01-28 01:57:30 +0000

    Added firebase db connection + .env

---
+++ src/firebase-config.ts
@@ -0,0 +1,34 @@
+// initalise app creates the connection between firebase and project
+
+import { initializeApp } from 'firebase/app';
+import {
+  getFirestore,
+  collection,
+  CollectionReference,
+  DocumentData,
+} from '@firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// pass app variable into getFireStore function, db will hold all the
+// firestore information
+
+export const db = getFirestore(app);
+
+// This is just a helper to add the type to the db responses
+const createCollection = <T = DocumentData>(collectionName: string) => {
+  return collection(db, collectionName) as CollectionReference<T>;
+};
+
+export const poemsCollection = createCollection<DocumentData>('poems-db');
