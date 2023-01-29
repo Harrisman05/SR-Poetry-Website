92f01f21b470cd4572ff924a18b1285b06d2839d
harle <harleyharris05@hotmail.com>
2023-01-29 12:31:23 +0000

    Added poem newline converter for upload to firestore collection.

---
+++ local-poems/newline_converter.js
@@ -0,0 +1,37 @@
+import * as fs from 'node:fs';
+
+/* How to use
+
+    See 1_Me_Name_Is_Seamus for reference
+
+    - copy and paste poem from word document into a new input txt file 
+    - add folder/input_filename.txt to ument into a new input txt file variable below
+    - for output file, add folder/output_filename.txt to variable
+    - Copy text in output file in poems-db collection, poem field in firebase
+
+
+    */
+
+const input_poem_filepath = '2_Tripe_Hounds_and_Dirt_Birds/Tripe_Hounds_And_Dirt_Birds_input.txt';
+const output_poem_filepath = '2_Tripe_Hounds_and_Dirt_Birds/Tripe_Hounds_And_Dirt_Birds_converted.txt';
+
+const readFile = fs.readFileSync(
+  input_poem_filepath,
+  'utf-8'
+);
+
+console.log(readFile);
+
+const arr = readFile.split('\n');
+
+console.log(arr);
+
+const newline_arr = arr.map((sentence) => sentence.replace('\r', '\\n'));
+
+console.log(newline_arr);
+
+const poem_rejoined = newline_arr.join('');
+
+console.log(poem_rejoined);
+
+fs.writeFileSync(output_poem_filepath, poem_rejoined, 'utf-8');
