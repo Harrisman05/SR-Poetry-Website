76a4aad26c8b4b80b8a98bf4865cd24f5bafeb1d
Harley Harris <harley.harris@digital.cabinet-office.gov.uk>
2023-01-30 23:59:35 +0000

    Created contact me form and setup FormSubmit api to handle request.

--- src/pages/ContactMe.tsx
+++ src/pages/ContactMe.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
-
 const ContactMe = () => {
   return (
-    <div>ContactMe</div>
-  )
-}
+    <div className='font-baskerville'>
+      <h1 className='text-center text-4xl my-10'>Contact Me</h1>
+      <form
+        action='https://formsubmit.co/'
+        method='POST'
+        className='w-9/12 max-w-sm mx-auto grid'
+      >
+        <div>
+          <label className='block mb-3'>Full Name:</label>
+          <input className='w-full mb-3 pl-1' type='text' name='name' required />
+        </div>
+        <div>
+          <label className='block mb-3'>Email:</label>
+          <input className='w-full mb-3 pl-1' type='text' name='email' required />
+        </div>
+        <div>
+          <label className='block mb-3'>Message:</label>
+          <textarea className='w-full h-28 mb-3 pl-1' name='message' required></textarea>
+        </div>
+        <button className='text-2xl hover:text-hyperlink-blue hover:underline' type='submit'>Send</button>
+      </form>
+    </div>
+  );
+};
 
-export default ContactMe
+export default ContactMe;
