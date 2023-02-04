93437b0626a73551278abe96169269d6f9f3419a
Harley Harris <harley.harris@digital.cabinet-office.gov.uk>
2023-02-04 16:45:22 +0000

    Added another PORT configuration option to vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 3030
   },
+  preview: {
+    port: 8080
+  }
 })
+
