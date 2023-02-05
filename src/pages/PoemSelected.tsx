d569fde1a8768467ae44ba4caaa750828cdb5a38
Harley Harris <harley.harris@digital.cabinet-office.gov.uk>
2023-02-05 09:38:39 +0000

    update main branch for vercel to detect change

--- src/pages/PoemSelected.tsx
+++ src/pages/PoemSelected.tsx
@@ -22,8 +22,6 @@ const PoemSelected = () => {
     import.meta.url
   ).href; // https://vitejs.dev/guide/assets.html#new-url-url-import-meta-url
 
-  const selected2 = meNameIsSeamus;
-
   console.log(location);
 
   // Adjusting Audio
