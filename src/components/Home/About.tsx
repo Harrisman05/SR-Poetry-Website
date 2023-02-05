8cb056dd7a6762e064848a443083557e8518e90c
Harley Harris <harley.harris@digital.cabinet-office.gov.uk>
2023-02-05 09:53:13 +0000

    Updated About text

--- src/components/Home/About.tsx
+++ src/components/Home/About.tsx
@@ -6,7 +6,7 @@ const About = () => {
     <div className='font-baskerville text-md md:text-lg'>
       <p>I'm Séamus, the London Irishman poet, trying to put a smile on the face of people I've never met. With words of humour, history and my take on the life I've lived and still living!</p>
       <br />
-      <p>Hoping to make people smile and ask the question why we should believe what we see!
+      <p>Hoping to inspire people to pick up a pen and write a poem!
       </p>
       <br />
       <p>Check out my poetry by <Link to='/poetry-index' className='text-hyperlink-blue hover:underline'>clicking here...</Link></p>
