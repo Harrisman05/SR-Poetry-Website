48b92f7f7f254c9bd670bb8560604c9fc0c9dacf
Harley Harris <harley.harris@digital.cabinet-office.gov.uk>
2023-02-06 21:36:00 +0000

    Added hard-coded relative positions to all navbar icons.

--- src/components/nav/NavBar.tsx
+++ src/components/nav/NavBar.tsx
@@ -7,36 +7,42 @@ import { NavLink } from 'react-router-dom';
 const NavBar = () => {
   return (
     <nav className='sticky flex w-auto justify-between bg-blue-200 px-2 pt-1 font-handlee'>
-      <div className='relative bg-slate-500'>
+      <div className='relative'>
         <NavLink
           to='/'
           className='text-sm hover:cursor-pointer hover:text-purple-700 hover:underline md:text-base lg:text-lg'
         >
-          <IoHome className='relative left-1/4 text-orange-900' />
+          <IoHome className='relative left-[30%] text-orange-400' />
           Home
         </NavLink>
       </div>
-      <NavLink
-        to='poetry-index'
-        className='align-center flex flex-col text-sm hover:cursor-pointer hover:text-purple-700 hover:underline md:text-base lg:text-lg'
-      >
-        <IoBookSharp className='w-full text-green-600' />
-        Poetry
-      </NavLink>
-      <NavLink
-        to='media'
-        className='align-center flex flex-col text-sm hover:cursor-pointer hover:text-purple-700 hover:underline md:text-base lg:text-lg'
-      >
-        <IoLogoYoutube className='w-full text-youtube-red' />
-        Media
-      </NavLink>
+      <div className='relative'>
+        <NavLink
+          to='poetry-index'
+          className='text-sm hover:cursor-pointer hover:text-purple-700 hover:underline md:text-base lg:text-lg'
+        >
+          <IoBookSharp className='relative left-1/3 text-green-600' />
+          Poetry
+        </NavLink>
+      </div>
+      <div className='relative'>
+        <NavLink
+          to='media'
+          className='text-sm hover:cursor-pointer hover:text-purple-700 hover:underline md:text-base lg:text-lg'
+        >
+          <IoLogoYoutube className='relative left-[31%] text-youtube-red' />
+          Media
+        </NavLink>
+      </div>
+      <div className='relative'>
       <NavLink
         to='contact-me'
-        className='align-center flex flex-col text-sm hover:cursor-pointer hover:text-purple-700 hover:underline md:text-base lg:text-lg'
+        className='text-sm hover:cursor-pointer hover:text-purple-700 hover:underline md:text-base lg:text-lg'
       >
-        <IoMailSharp className='w-full text-gray-600' />
+        <IoMailSharp className='relative left-[40%] text-gray-600' />
         Contact me
       </NavLink>
+      </div>
     </nav>
   );
 };
