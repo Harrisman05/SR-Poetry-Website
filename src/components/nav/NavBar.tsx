84cab282c121f1af32d7ee55c3cd73b3681b0325
Harley Harris <harley.harris@digital.cabinet-office.gov.uk>
2023-02-06 21:12:49 +0000

    re-coloured for debugging

--- src/components/nav/NavBar.tsx
+++ src/components/nav/NavBar.tsx
@@ -7,12 +7,12 @@ import { NavLink } from 'react-router-dom';
 const NavBar = () => {
   return (
     <nav className='sticky flex w-auto justify-between bg-blue-200 px-2 pt-1 font-handlee'>
-      <div className='align-center flex flex-col'>
+      <div className='bg-slate-500'>
         <NavLink
           to='/'
           className='text-sm hover:cursor-pointer hover:text-purple-700 hover:underline md:text-base lg:text-lg'
         >
-          <IoHome className='w-full text-orange-400' />
+          <IoHome className='w-full text-orange-900' />
           Home
         </NavLink>
       </div>
