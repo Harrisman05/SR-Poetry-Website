499f3e049df0c2f19343cfac2cf43428f73ba90a
Harley Harris <harley.harris@digital.cabinet-office.gov.uk>
2023-02-07 08:02:48 +0000

    Added responsiveness to Media Page

--- src/pages/Media.tsx
+++ src/pages/Media.tsx
@@ -8,22 +8,31 @@ const Media = () => {
       <p className='border-b-2 border-dashed border-black pb-6 text-center text-lg md:text-xl'>
         You can find me on various social media platforms:
       </p>
-      <div className='mx-auto py-4 grid grid-cols-2 gap-x-10 gap-y-8 items-center bg-slate-300 text-center'>
-        <a href='https://www.youtube.com/@Seamus_the_Poet' target="_blank" className='flex items-center justify-between cursor-pointer hover:text-hyperlink-blue hover:underline'>
-          <p className='font-youtube-sans text-3xl font-bold text-hyperlink-blue'>YouTube</p>
-          <YouTubeIcon className='text-youtube-red hover:brightness-125' sx={{ fontSize: 70 }} />
+      <div className='mx-auto grid grid-cols-2 items-center gap-x-3 gap-y-4  lg:gap-x-10 lg:gap-y-8 py-4 text-center'>
+        <a
+          href='https://www.youtube.com/@Seamus_the_Poet'
+          target='_blank'
+          className='flex cursor-pointer items-center justify-between hover:text-hyperlink-blue hover:underline'
+        >
+          <p className='font-youtube-sans text-xl lg:text-3xl font-bold text-hyperlink-blue'>
+            YouTube
+          </p>
+          <YouTubeIcon
+            className='text-youtube-red lg:scale-150 hover:brightness-125'
+            sx={{ fontSize: 50 }}
+          />
         </a>
-        <a href='https://www.youtube.com/@Seamus_the_Poet' className='text-2xl text-hyperlink-blue hover:underline'>
+        <a
+          href='https://www.youtube.com/@Seamus_the_Poet'
+          className='lg:text-2xl text-hyperlink-blue hover:underline'
+        >
           @Seamus_the_Poet
         </a>
         <div className='flex items-center justify-between gap-3'>
-          <p className='text-3xl font-roboto'>Instagram</p>
-          <InstagramIcon
-          className='text'
-          sx={{fontSize: 70}}
-          />
+          <p className='font-roboto text-xl lg:text-3xl'>Instagram</p>
+          <InstagramIcon className='lg:scale-150' sx={{ fontSize: 50 }} />
         </div>
-        <p className='text-2xl'>Coming soon...</p>
+        <p className='lg:text-2xl'>Coming soon...</p>
       </div>
     </section>
   );
