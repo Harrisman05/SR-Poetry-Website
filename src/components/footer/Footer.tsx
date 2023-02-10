d2dfbd4c45a57dc7e576f86ba7c687585a9445a9
harle <harleyharris05@hotmail.com>
2023-02-10 22:18:55 +0000

    Increased footer text size

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className='w-auto mt-auto py-1 bg-blue-200 text-center text-[6px]'>© 2023 Seamus Rattigan's seamusrattigan.com. All rights reserved</footer>
+    <footer className='w-auto mt-auto py-1 bg-blue-200 text-center text-[12px]'>© 2023 Seamus Rattigan's seamusrattigan.com. All rights reserved</footer>
   )
 }
 
