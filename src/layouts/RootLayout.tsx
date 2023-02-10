a634a28de2b5e8a5167cfcf5704f7c6d152009cb
harle <harleyharris05@hotmail.com>
2023-02-10 22:17:57 +0000

    Removed horizontal scrollbar

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -4,8 +4,8 @@ import Footer from '../components/footer/Footer';
 
 const RootLayout = () => {
   return (
-    <div className='root-layout w-screen bg-orange-50'>
-      <div className='absolute left-0 s:h-[38px] md:h-[44px] lg:h-[50px] w-screen bg-blue-200'></div>
+    <div className='root-layout bg-orange-50'>
+      <div className='absolute left-0 s:h-[38px] md:h-[44px] lg:h-[50px] w-full bg-blue-200'></div>
       <div className='mx-auto flex min-h-[calc(100vh-17px)] max-w-[800px] flex-col'>
         <header>
           <NavBar />
